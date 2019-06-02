3705ab7001657d4743ce747daf01fb4db3c5d272
TheGreatCodingEmperor <thehandsomeboyno.1@gmail.com>
2019-06-02 19:46:45 +0800

    milestone2:delete finish

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const key = require('./privateSettings.json');
 
 // TODO(you): Change the value of this string to the spreadsheet id for your
 // GSA spreadsheet. See HW5 spec for more information.
-const SPREADSHEET_ID = '__YOUR__SPREADSHEET__ID__HERE__';
+const SPREADSHEET_ID = '1IoLg4dTdrMHf3KZMXURTEYsNIZCgTFCgr9sqHUK1S-M';
 
 const app = express();
 const jsonParser = bodyParser.json();
@@ -18,19 +18,34 @@ async function onGet(req, res) {
   const result = await sheet.getRows();
   const rows = result.rows;
   console.log(rows);
+  const keys = rows[0];
+  const data = [];
+  for(i=1;i<rows.length;i++){
+    const tmp={};
+    key1=rows[0][0];
+    key2=rows[0][1];
+    tmp[key1]=rows[i][0];
+    tmp[key2]=rows[i][1];
+    data.push(tmp);
+  }
 
   // TODO(you): Finish onGet.
 
-  res.json( { status: 'unimplemented'} );
+  res.json( data );
 }
 app.get('/api', onGet);
 
 async function onPost(req, res) {
+  const result = await sheet.getRows();
+  const rows = result.rows;
+
   const messageBody = req.body;
-
   // TODO(you): Implement onPost.
+  console.log(messageBody);
+  const keys = Object.getOwnPropertyNames(messageBody);
+  for(let i=0;i<keys.length;i++)
 
-  res.json( { status: 'unimplemented'} );
+  res.json( { status: 'successed'} );
 }
 app.post('/api', jsonParser, onPost);
 
@@ -39,19 +54,42 @@ async function onPatch(req, res) {
   const value  = req.params.value;
   const messageBody = req.body;
 
+
   // TODO(you): Implement onPatch.
 
-  res.json( { status: 'unimplemented'} );
+  res.json( { status: 'successed'} );
 }
 app.patch('/api/:column/:value', jsonParser, onPatch);
 
 async function onDelete(req, res) {
+  const result = await sheet.getRows();
+  const rows = result.rows;
   const column  = req.params.column;
   const value  = req.params.value;
+  let delete_row = -1;
+  for(let i=0;i<rows.length;i++){
+    if(rows[0][0]===column){
+      if(rows[i][0]===value){
+        delete_row=i;
+        break;
+      }
+    }
+    else{
+      if(rows[i][1]===value){
+        delete_row=i;
+        break;
+      }
+    }
+  }
+  if(delete_row!==-1){
+    await sheet.deleteRow(delete_row);
+  }
+  
+  
 
   // TODO(you): Implement onDelete.
 
-  res.json( { status: 'unimplemented'} );
+  res.json( { status: 'successed'} );
 }
 app.delete('/api/:column/:value',  onDelete);
 
